e6f775905cd6323ca0ac91a0c474ac23f3ce96f9
Puppycoder88 <jaingarima1795@gmail.com>
2024-06-27 15:31:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SlotM from "./SlotMach";
+
+const App = () => {
+  return (<>
+  <h1 className="heading_style">🎰Welcome to <span style={{ fontWeight:"bold"}}>Slot machine game 🎰</span>
+   </h1>
+   <div className="slotmachine">
+   <SlotM  x="😄" y="😄" z="😄"/>
+   <hr/>
+   <SlotM  x="😄" y="🎅" z="😄"/>
+   <hr/>
+   <SlotM  x="🍉" y="🍎" z="🍌"/>
+   <hr/>
+   <SlotM  x="💑" y="💑" z="💑"/>
+   </div>
+   </>
+  );
+};
+
+export default App;
